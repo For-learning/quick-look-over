8a0d936556da06fb715073cb51059b72579c9c85
Crown <crown_0726@163.com>
2017-07-22 08:00:18 +0800

    Initialize pro.

---
+++ Qlookover/tip.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+REM  QBFC Project Options Begin
+REM  HasVersionInfo: No
+REM  Companyname: 
+REM  Productname: 
+REM  Filedescription: 
+REM  Copyrights: 
+REM  Trademarks: 
+REM  Originalname: 
+REM  Comments: 
+REM  Productversion:  0. 0. 0. 0
+REM  Fileversion:  0. 0. 0. 0
+REM  Internalname: 
+REM  Appicon: 
+REM  AdministratorManifest: No
+REM  QBFC Project Options End
+@ECHO ON
+@echo off
+
+if %var%==2 (
+  start "title" /max .\go.txt
+  setx var 1
+)else (
+  taskkill /f /im notepad.exe
+  setx var 2
+)
